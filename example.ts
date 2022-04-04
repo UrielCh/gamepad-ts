d749f7d1488aa8b861805cbe8043f48fa856238a
UrielCh <uriel.chemouni@gmail.com>
2022-04-04 10:02:52 +0300

    add typings

---
+++ example.ts
@@ -0,0 +1,18 @@
+import GamePadController from './src/index';
+const ctrl = new GamePadController({brand: 'playStation'});
+/*
+ * A,B,X,Y buttons
+ */
+ctrl.on('LStick', (pos) => console.log(`L: ${pos[0]}, ${pos[1]}`));
+ctrl.on('RStick', (pos) => console.log(`R: ${pos[0]}, ${pos[1]}`));
+ctrl.on('dpad', (pos) => console.log(`dPad: ${pos[0]}, ${pos[1]}`));
+ctrl.on('square', (pressed: boolean) => console.log('square: ' + pressed));
+ctrl.on('cross', (pressed: boolean) => console.log('cross: ' + pressed));
+ctrl.on('circle', (pressed: boolean) => console.log('circle: ' + pressed));
+ctrl.on('triangle', (pressed: boolean) => console.log('triangle: ' + pressed));
+/*
+ * Setup
+ */
+ctrl.on('connected', () => console.log('Xbox controller connected'));
+ctrl.on('error', (err) => console.error(err));
+ctrl.start();
